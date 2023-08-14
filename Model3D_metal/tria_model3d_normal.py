normalized_obs not working bug in stable_baselines3 removed normalization!?

Model3D_metal/tria_model3d_normal.py
@@ -64,7 +64,7 @@ print('action space sample: ', env_s.action_space.sample())
 
 env_s = DummyVecEnv([lambda: env_s])
 
-env_s = VecNormalize(env_s, training=True, norm_obs=True, norm_reward=True, epsilon=1e-08, gamma=0.99)
+#env_s = VecNormalize(env_s, training=True, norm_obs=True, norm_reward=True, epsilon=1e-08, gamma=0.99)
 
 log_path = os.path.join('train', 'log')
 
@@ -79,7 +79,7 @@ model = A2C(policy = "MlpPolicy",
             ent_coef = 1.185e-08,#0.00294,#3.11e-06,#0.00037,#1.0976520036433521e-08,#0.04553259441269758,#0.0,
             policy_kwargs=dict(
             activation_fn = nn.Tanh,  
-            net_arch = {'pi':[64, 64, 64], 'vf':[64, 64, 64]},  
+            net_arch = {'pi':[128, 128, 128], 'vf':[128, 128, 128]},  
             log_std_init=-2, 
             ortho_init=False
             ),

release/server/inference.py
@@ -1,5 +1,6 @@
 import os
 import gym
+import numpy as np
 from stable_baselines3 import A2C
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 import tria_rl
@@ -16,20 +17,21 @@ print('action space sample:      ', env_s.action_space.sample())
 
 env_s = DummyVecEnv([lambda: env_s])
 
-env_s = VecNormalize(env_s, training=True, norm_obs=True, norm_reward=True, epsilon=1e-08, gamma=0.99)
+env_n = VecNormalize(env_s, training=True, norm_obs=True, norm_reward=True, epsilon=1e-08, gamma=0.99)
 
 log_path = os.path.join('../', 'logs')
 
 tria_a2c_model_path = os.path.join('../','model', "tria_a2c_normalized")
 
-model = A2C.load(tria_a2c_model_path, env=env_s)
+model = A2C.load(tria_a2c_model_path, env=env_n)
 
 #
-observation_o = env_s.reset()
-ob = env_s.get_original_obs().ravel().tolist()
-observation_api = env_s.normalize_obs(ob)
-action_o = model.predict(observation_o, deterministic=True)
-print('norm obs: {} unnorm obs: {} apinorm obs: {} action: {}'.format(observation_o, ob, observation_api, action_o))
+#observation_o = env_s.reset()
+observation_ext = np.array( (4.,  5., 6.)) 
+#ob =  env_s.get_original_obs().ravel().tolist()
+observation_api = env_n.normalize_obs(observation_ext).ravel().tolist()
+action_o = model.predict(observation_api, deterministic=True)
+print('unnorm obs: {} apinorm obs: {} action: {}'.format(observation_ext, observation_api, action_o))
 #
 env_s.close() 
 

release/server/tria_inference.py
@@ -14,35 +14,37 @@ class tria_inference_engine():
         self.model_name = model_name
         self.model_path = os.path.join(root_path, model_path, model_name)
         self.log_path = os.path.join(root_path, log_path) 
-        self.env = None
         self.model = None
 
     def loadEnvironment(self):
-        self.env = gym.make(self.env_id)
+        env_raw = gym.make(self.env_id)
+        return env_raw
 
-    def loadNormalizedEnv(self):    
-        self.env = DummyVecEnv([lambda: self.env])
-        self.env = VecNormalize(self.env, training=True, norm_obs=True, norm_reward=True, epsilon=1e-08, gamma=0.99)
-
-    def showEnvionmentProperties(self):
+    def loadNormalizedEnv(self, env):    
+        env_vec = DummyVecEnv([lambda: env])
+        env_norm = VecNormalize(env_vec, training=True, norm_obs=True, norm_reward=True, epsilon=1e-08, gamma=0.99)
+        return env_norm
+    
+    def showEnvionmentProperties(self, env):
         print('* * * Tria Enviromnet for tria device controll predictions * * *')
-        print('>> observation space size:   ',self.env.observation_space.shape[0])
-        print('>> observation space sample: ',self.env.observation_space.sample)
-        print('>> action space:             ', self.env.action_space)
-        print('>> action space sample:      ', self.env.action_space.sample())    
+        print('>> observation space size:   ',env.observation_space.shape[0])
+        print('>> observation space sample: ',env.observation_space.sample)
+        print('>> action space:             ',env.action_space)
+        print('>> action space sample:      ',env.action_space.sample())    
 
-    def loadModel(self):
-     self.model = A2C.load(self.model_path, env=self.env)
-     self.env.close()
+    def loadModel(self, env):
+     self.model = A2C.load(self.model_path, env=env)
 
-    def getActionPrediction(self, obs):
-       #obs= np.array(obs, dtype=np.float32)
-       print('original observation: ', obs)
-       #observation_api = self.env.normalize_obs(obs)  
-       #print('normalized observation', obs)
-       action_o = self.model.predict(obs, deterministic=True)[0]
-       print(action_o)
-       return action_o
+    def getActionPrediction(self, env, obs):
+       retDict = {}
+       obs = np.array([obs])
+       print('>>orig obs:', obs)
+       observation_api = env.normalize_obs(obs)  
+       print('>>norm obs:', observation_api)
+       action_o = self.model.predict(observation_api, deterministic=True)[0]
+       retDict['action'] = action_o.item()
+       print('>>action  :',retDict)
+       return retDict
 
 
 if __name__ == '__main__':
@@ -53,14 +55,12 @@ if __name__ == '__main__':
                                 'TriaClimate-v0',
                                 'tria_a2c_normalized'
                                 )
-   tie.loadEnvironment()
+   env_r = tie.loadEnvironment()
 
-   tie.showEnvionmentProperties()
+   tie.showEnvionmentProperties(env_r)
 
-   tie.loadNormalizedEnv()
+   env_n = tie.loadNormalizedEnv(env_r)
 
-   tie.loadModel()
+   tie.loadModel(env_n)
 
-   
-
-   print('action: ', tie.getActionPrediction(np.array([59.78,57.89,20.976], dtype=np.float32))[0] )
+   print('action: ', tie.getActionPrediction(env_n, np.array([[59.78,57.89,20.976]], dtype=np.float32)) )