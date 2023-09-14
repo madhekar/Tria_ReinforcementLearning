3eea23aae70076038718a7c0f2fce71a2fbc8418
Bhalchandra Madhekar <bmadhekar@gmail.com>
2023-09-14 08:55:10 -0700

    testing RL integration

--- release/communicator/tria_communicator.py
+++ release/communicator/tria_communicator.py
@@ -13,18 +13,15 @@ if __name__ == '__main__':
     rpl = RPL(http, baseUrl)
 
     #print('Running. Press CTRL-C to exit.')
-    with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as tria:
+    with serial.Serial("/dev/ttyAMA0", 9600, timeout=1) as tria:
         time.sleep(0.1) #wait for serial to open
         if tria.isOpen():
             print("{} connected!".format(tria.port))
             try:
                 while True:
-                    #cmd=input("Enter command : ")
-                    #tria.write(cmd.encode())
-                    #time.sleep(0.1) #wait for tria to answer
                     while tria.inWaiting()==0: pass
                     if  tria.inWaiting()>0: 
-                        observations=tria.readline()
+                        observations=tria.readline().decode('utf-8').rstrip()
                         print('observation: ',observations)
                         tria.flushInput() #remove data after reading
                         obs = observations.split(':')
